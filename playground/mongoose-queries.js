f71e9448aa6066f2e48c3de900dfb059f3ee1e69
Devon Lambert <devon@realityknights.com>
2018-10-23 17:19:16 -0500

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,44 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = "5bcf97a5ee19735c2bcbb074";
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//
+//   console.log('Todo By ID', todo);
+// }).catch((e) => { console.log(e) });
+
+// User.findById
+var userId = '5bcd06cb3a31216d1f6c3f8c';
+
+User.findById(userId).then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+}).catch((e) => { console.log('User not found', e) })
