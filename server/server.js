d4b4e64c49c42b2ef9e515b6262c4ce55fd2228c
Devon Lambert <devon@realityknights.com>
2018-10-24 11:24:25 -0500

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -32,7 +32,6 @@ app.get('/todos', (req, res) => {
   });
 });
 
-// GET /todos/12343212
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -50,6 +49,25 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send()
   });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(port, () => {
