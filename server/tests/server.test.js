5e8592847cc75a314a7df2bdef6d099754c1411e
Devon Lambert <devon@realityknights.com>
2018-10-26 16:11:54 -0500

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }]
 
 beforeEach((done) => {
@@ -81,7 +83,7 @@ describe('GET /todos/:id', () => {
       .get(`/todos/${todos[0]._id.toHexString()}`)
       .expect(200)
       .expect((res) => {
-        expect(res.body.todo.text).toBe(todos[0].text)
+        expect(res.body.todo.text).toBe(todos[0].text);
       })
       .end(done);
   });
@@ -132,6 +134,52 @@ describe('DELETE /todos/:id', () => {
     request(app)
       .delete('/todos/1234')
       .expect(404)
+      .end(done);
+  });
+});
+
+describe('PATCH /todos/:id', () => {
+
+
+  it('should update the todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var text = 'Some new test text';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+          text,
+          completed: true
+        })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    // grab id of second todo item
+    var hexId = todos[1]._id.toHexString();
+    var text = 'Some new test case text';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+    // update text, set completed to false
+      .send({
+        text,
+        completed: false
+      })
+    // 200
+      .expect(200)
+    // text is changed, completed false, completedAt is null .toNotExist()
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.complededAt).toNotExist();
+      })
       .end(done);
   });
 });
